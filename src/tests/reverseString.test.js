578ee8be28e7a11ae425010af582406d63a89e9c
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-11-03 18:03:05 +0530

    test: add 3 tests for reverseString

--- src/tests/reverseString.test.js
+++ src/tests/reverseString.test.js
@@ -1,4 +1,17 @@
 import reverseString from "../modules/reverseString";
+
 test("abc returns cba", () => {
   expect(reverseString("abc")).toBe("cba");
+});
+
+test("hello world returns dlrow olleh", () => {
+  expect(reverseString("hello world")).toBe("dlrow olleh");
+});
+
+test(`"12345" returns "54321"`, () => {
+  expect(reverseString("12345")).toBe("54321");
+});
+
+test("12345 returns Not a String", () => {
+  expect(reverseString(12345)).toBe("Not a String");
 });
