cc849a1164dc44094bdbed2dcb6ca47b30f65ef9
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-11-03 18:43:59 +0530

    feat: add subtract tests

--- src/modules/calculator.js
+++ src/modules/calculator.js
@@ -10,8 +10,17 @@ const add = (num1, num2) => {
   }
 };
 
+const subtract = (num1, num2) => {
+  if (isValidNumber(num1) && isValidNumber(num2)) {
+    return num1 - num2;
+  } else {
+    return "Not valid Number(s)";
+  }
+};
+
 const calculator = {
   add,
+  subtract,
 };
 
 export default calculator;
