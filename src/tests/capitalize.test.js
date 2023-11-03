6e365894416378f847df6b00adb2fa38083be9d5
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-11-03 12:54:33 +0530

    test: add empty string test for capitalize suite

--- src/tests/capitalize.test.js
+++ src/tests/capitalize.test.js
@@ -3,3 +3,7 @@ import capitalize from "../modules/capitalize";
 test("Hello", () => {
   expect(capitalize("hello")).toBe("Hello");
 });
+
+test("Empty string", () => {
+  expect(capitalize("")).toBe("Empty String");
+});
