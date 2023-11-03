9436ee8a71e6ddaa7a457ffdcfd9747ab8f96f91
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-11-03 18:47:06 +0530

    fix: change test titles

--- src/tests/calculator.test.js
+++ src/tests/calculator.test.js
@@ -1,10 +1,10 @@
 import calculator from "../modules/calculator";
 
-test("calculator.add(2,3) returns 5", () => {
+test("add integer numbers", () => {
   expect(calculator.add(2, 3)).toBe(5);
 });
 
-test("calculator.add(hello,3) returns Not valid Number(s)", () => {
+test("add string with number", () => {
   expect(calculator.add("hello", 3)).toBe("Not valid Number(s)");
 });
 
@@ -12,11 +12,11 @@ test("add decimal numbers", () => {
   expect(calculator.add(3.5, 3)).toBe(6.5);
 });
 
-test("calculator.subtract(2,3) returns -1", () => {
+test("subtract integer numbers", () => {
   expect(calculator.subtract(2, 3)).toBe(-1);
 });
 
-test("calculator.subtract(hello,3) returns Not valid Number(s)", () => {
+test("subtract string with number", () => {
   expect(calculator.subtract("hello", 3)).toBe("Not valid Number(s)");
 });
 
