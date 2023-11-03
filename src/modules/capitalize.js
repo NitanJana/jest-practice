e5531067eccb99f59b9c467710313abaf23f41f5
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-11-03 13:04:29 +0530

    fix: add String validity check

--- src/modules/capitalize.js
+++ src/modules/capitalize.js
@@ -1,4 +1,5 @@
 export default function capitalize(string) {
   if (!string) return "Empty String";
+  if (typeof (string) !== 'string') return "Not valid String";
   return string[0].toUpperCase()+string.slice(1);
 }
