1ab5a741a81d939b0038aa63fe14b174f99799a8
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-11-03 18:41:30 +0530

    test: add tests for subtract

--- src/tests/calculator.test.js
+++ src/tests/calculator.test.js
@@ -11,3 +11,15 @@ test("calculator.add(hello,3) returns Not valid Number(s)", () => {
 test("add decimal numbers", () => {
   expect(calculator.add(3.5, 3)).toBe(6.5);
 });
+
+test("calculator.subtract(2,3) returns -1", () => {
+  expect(calculator.subtract(2, 3)).toBe(-1);
+});
+
+test("calculator.subtract(hello,3) returns Not valid Number(s)", () => {
+  expect(calculator.subtract("hello", 3)).toBe("Not valid Number(s)");
+});
+
+test("subtract decimal numbers", () => {
+  expect(calculator.subtract(3.5, 3)).toBe(0.5);
+});
