3e4a9623ffaab4ac6606cfbb689499d5523fe41d
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-11-03 21:02:11 +0530

    feat: check for array validity

--- src/modules/analyzeArray.js
+++ src/modules/analyzeArray.js
@@ -11,15 +11,23 @@ const calcAverage = (array) => {
 };
 
 const calcMin = (array) => {
-  return array.reduce((prev,curr)=>Math.min(prev,curr))
+  return array.reduce((prev, curr) => Math.min(prev, curr));
 };
 
 const calcMax = (array) => {
-  return array.reduce((prev,curr)=>Math.max(prev,curr))
+  return array.reduce((prev, curr) => Math.max(prev, curr));
+};
 
+const isValidArray = (array) => {
+  if (!Array.isArray(array)) return false;
+
+  return array.every((item) => typeof item === "number");
 };
 
 const analyzeArray = (array) => {
+  if (!isValidArray(array)) {
+    return "Not a valid Array";
+  }
   const analyzedArray = {
     average: calcAverage(array),
     min: calcMin(array),
--- src/tests/analyzeArray.test.js
+++ src/tests/analyzeArray.test.js
@@ -8,3 +8,19 @@ test("Array of Numbers", () => {
     length: 6,
   });
 });
+
+test("Array of Strings", () => {
+  expect(analyzeArray(["Hello", "World"])).toBe("Not a valid Array");
+});
+
+test("Mixed Array", () => {
+  expect(analyzeArray([5, "Hello", 12, "World"])).toBe("Not a valid Array");
+});
+
+test("String", () => {
+  expect(analyzeArray("Hello")).toBe("Not a valid Array");
+});
+
+test("Number", () => {
+  expect(analyzeArray(45452)).toBe("Not a valid Array");
+});
