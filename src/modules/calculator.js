c3f297534bc880fb09d0d76031d1612ccba7d2d4
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-11-03 18:37:02 +0530

    feat: add add method

---
+++ src/modules/calculator.js
@@ -0,0 +1,17 @@
+const isValidNumber = (number) => {
+  return typeof number === "number";
+};
+
+const add = (num1, num2) => {
+  if (isValidNumber(num1) && isValidNumber(num2)) {
+    return num1 + num2;
+  } else {
+    return "Not valid Number(s)";
+  }
+};
+
+const calculator = {
+  add,
+};
+
+export default calculator;
