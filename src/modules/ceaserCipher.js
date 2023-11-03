9b39d819cab9a8cc3c95252bc73651abd3b295e8
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-11-03 20:15:04 +0530

    feat: add tests and solution for ceaserCipher

--- src/modules/ceaserCipher.js
+++ src/modules/ceaserCipher.js
@@ -1,12 +1,32 @@
-const encipher = (string, key) => {
-  let cipheredString = "";
-  for (let i = 0; i < string.length; i++) {
-    cipheredString += String.fromCharCode(string[i].charCodeAt() + key);
+const upperCipher = (charCode, key) => {
+  return String.fromCharCode(((charCode + key - 65) % 26) + 65);
+};
+const lowerCipher = (charCode, key) => {
+  return String.fromCharCode(((charCode + key - 97) % 26) + 97);
+};
+
+const encipher = (text, key) => {
+  let cipheredtext = "";
+
+  for (let i = 0; i < text.length; i++) {
+    if (text.charAt(i).match(/[a-zA-Z]/)) {
+      let code = text.charAt(i).charCodeAt();
+      if (code >= 65 && code <= 90) {
+        cipheredtext += upperCipher(code, key);
+      }
+      else if (code >= 97 && code <= 122) {
+        cipheredtext += lowerCipher(code, key);
+      }
+    }
+    else {
+      cipheredtext += text.charAt(i);
+    }
   }
-  return cipheredString;
+  return cipheredtext;
 };
 
 const ceaserCipher = {
   encipher,
 };
 export default ceaserCipher;
+
--- src/tests/ceaserCipher.test.js
+++ src/tests/ceaserCipher.test.js
@@ -3,3 +3,23 @@ import ceasrCipher from "../modules/ceaserCipher";
 test("normal string", () => {
   expect(ceasrCipher.encipher("hello", 6)).toBe("nkrru");
 });
+
+test("character wraps around after z", () => {
+  expect(ceasrCipher.encipher("xerox", 12)).toBe("jqdaj");
+});
+
+test("character stays the same case", () => {
+  expect(ceasrCipher.encipher("Hello", 6)).toBe("Nkrru");
+});
+
+test("key size wraps around 26", () => {
+  expect(ceasrCipher.encipher("hello", 36)).toBe("rovvy");
+});
+
+test("punctuations stays same", () => {
+  expect(ceasrCipher.encipher("hello,World", 36)).toBe("rovvy,Gybvn");
+});
+
+test("Numbers stays same", () => {
+  expect(ceasrCipher.encipher("hello,World 255", 36)).toBe("rovvy,Gybvn 255");
+});
