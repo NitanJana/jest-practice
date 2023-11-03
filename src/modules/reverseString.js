f3923435cf306ffc4d4872d30eefcd38d5adfefa
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-11-03 18:05:00 +0530

    feat: add string check

--- src/modules/reverseString.js
+++ src/modules/reverseString.js
@@ -1,4 +1,5 @@
 export default function reverseString(string) {
+  if (typeof string !== "string") return "Not a String";
   let reversedString = "";
   for (let i = string.length - 1; i >= 0; i -= 1) {
     reversedString += string[i];
