41b04ed3df9765798ce1ec2ab7acb5a054903fe1
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-11-03 18:57:27 +0530

    feat: add divide method

--- src/modules/calculator.js
+++ src/modules/calculator.js
@@ -26,10 +26,19 @@ const multiply = (num1, num2) => {
   }
 };
 
+const divide = (num1, num2) => {
+  if (isValidNumber(num1) && isValidNumber(num2)) {
+    return parseFloat((num1 / num2).toFixed(2));
+  } else {
+    return "Not valid Number(s)";
+  }
+};
+
 const calculator = {
   add,
   subtract,
-  multiply
+  multiply,
+  divide
 };
 
 export default calculator;
